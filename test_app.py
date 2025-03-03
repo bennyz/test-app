7b7c28509ecfb782807d145ab1035d765e641255
Benny Zlotnik <bzlotnik@protonmail.com>
2025-03-03 20:15:49 +0200

    don't fail

--- test_app.py
+++ test_app.py
@@ -4,5 +4,5 @@ def test_add_success():
     assert add(2, 2) == 4
 
 # Uncomment to make tests fail
-def test_add_failure():
-    assert add(2, 2) == 5
+# def test_add_failure():
+#     assert add(2, 2) == 5
