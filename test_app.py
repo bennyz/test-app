aab06d5863925e84bb22949301f84104ac751c24
Benny Zlotnik <bzlotnik@protonmail.com>
2025-03-03 20:27:37 +0200

    try webhook

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@ from app import add
 def test_add_success():
     assert add(2, 2) == 4
 
-# Uncomment to make tests fail
+# uncomment to make tests fail
 # def test_add_failure():
 #     assert add(2, 2) == 5
